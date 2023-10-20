c283977beb5aab74f8240564d92ded2eeb6365d2
Slider0007 <115730895+Slider0007@users.noreply.github.com>
2023-10-20 12:59:10 +0200

    chore: Update static part of firmware version string (#80)

--- code/main/version.h
+++ code/main/version.h
@@ -41,7 +41,7 @@ const char* libfive_git_branch(void)
 std::string getFwVersion(void) {
     std::string buf;
     if (std::string(GIT_TAG) == "") { // Tag not set, show branch
-        buf = "Development-Branch: " + std::string(GIT_BRANCH);
+        buf = "Develop: " + std::string(GIT_BRANCH);
     }
     else { // Tag is set, ignore branch
         buf = "Release: " + std::string(GIT_TAG);
